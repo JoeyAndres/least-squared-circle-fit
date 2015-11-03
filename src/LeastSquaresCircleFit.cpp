d76e2bdc7b949b34405be033837bc194d0ec87eb
Joey Arnold <jandres@ualberta.ca>
2015-11-02 21:09:07 -0700

    jenkins runnable

---
+++ src/LeastSquaresCircleFit.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jandres on 02/11/15.
+//
+
+#include "LeastSquaresCircleFit.h"
---
+++ tests/base_test.cpp
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+#include <gtest/gtest.h>
+
+TEST(LeastSquareCircleFitTest, BaseCase1) {
+
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+    std::cout << "Running main() from base_test.cpp" << std::endl;
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
