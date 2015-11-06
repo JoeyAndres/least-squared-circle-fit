89352e57d73b4d756a705bcd5e4154788fd84023
Joey Arnold <jandres@ualberta.ca>
2015-11-06 04:22:03 -0700

    All tests are passing

--- includes/LeastSquaresCircleFit.h
+++ includes/LeastSquaresCircleFit.h
@@ -9,41 +9,74 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <armadillo>
+
+#include <iostream>
 
 #include "const.h"
 
 namespace lscf {
     std::tuple<Radius, V2d> least_square_circle_fit(const std::vector<V2d>& v2ds) {
-        std::vector<double> u;
-        std::transform(v2ds.begin(), v2ds.end(), u.begin(), [](const V2d& v2d) {
+        size_t data_size = v2ds.size();
+
+        std::vector<double> x(data_size);
+        std::transform(v2ds.begin(), v2ds.end(), x.begin(), [](const V2d& v2d) {
             return std::get<0>(v2d);
         });
-
-        std::vector<double> v;
-        std::transform(v2ds.begin(), v2ds.end(), v.begin(), [](const V2d& v2d) {
+        std::vector<double> y(data_size);
+        std::transform(v2ds.begin(), v2ds.end(), y.begin(), [](const V2d& v2d) {
             return std::get<1>(v2d);
         });
 
-        double u_avg = std::accumulate(u.begin(), u.end(), 0, [](double sum, double cur_val) {
+        double x_avg = std::accumulate(x.begin(), x.end(), 0.0F, [](double sum, double cur_val) {
             return sum + cur_val;
+        }) / data_size;
+        double y_avg = std::accumulate(y.begin(), y.end(), 0.0F, [](double sum, double cur_val) {
+            return sum + cur_val;
+        }) / data_size;
+
+        std::vector<double> u(data_size);
+        for (size_t i = 0; i < data_size; i++) { u[i] = x[i] - x_avg; }
+        std::vector<double> v(data_size);
+        for (size_t i = 0; i < data_size; i++) { v[i] = y[i] - y_avg; }
+
+        double s_uu = std::accumulate(u.begin(), u.end(), 0.0F, [](double sum, double cur_val) {
+            return sum + cur_val * cur_val;
         });
-        double v_avg = std::accumulate(v.begin(), v.end(), 0, [](double sum, double cur_val) {
-            return sum + cur_val;
+        double s_uuu = std::accumulate(u.begin(), u.end(), 0.0F, [](double sum, double cur_val) {
+            return sum + cur_val * cur_val * cur_val;
+        });
+        double s_vv = std::accumulate(v.begin(), v.end(), 0.0F, [](double sum, double cur_val) {
+            return sum + cur_val * cur_val;
+        });
+        double s_vvv = std::accumulate(v.begin(), v.end(), 0.0F, [](double sum, double cur_val) {
+            return sum + cur_val * cur_val * cur_val;
         });
 
-        double s_uu = std::accumulate(u.begin(), u.end(), 0, [](double sum, double cur_val) {
-            return sum + cur_val * cur_val;
-        });
-        double s_uuu;
-        double s_vv = std::accumulate(v.begin(), v.end(), 0, [](double sum, double cur_val) {
-            return sum + cur_val * cur_val;
-        });
-        double s_vvv;
-        double s_uvv;
-        double s_vuu;
-        double v_c;
-        double u_c;
-        return std::make_tuple(0.0F, V2d({0.0F, 0.0F}));
+        double s_uv = 0.0F;
+        for (size_t i = 0; i < data_size; i++) { s_uv += u[i] * v[i]; }
+        double s_uvv = 0.0F;
+        for (size_t i = 0; i < data_size; i++) { s_uvv += u[i] * v[i] * v[i]; }
+        double s_vuu = 0.0F;
+        for (size_t i = 0; i < data_size; i++) { s_vuu += v[i] * u[i] * u[i]; }
+
+        auto b_1 = (s_uuu + s_uvv) * 0.5F;
+        auto b_2 = (s_vvv + s_vuu) * 0.5F;
+
+        arma::mat A = { { s_uu, s_uv },
+                        { s_uv, s_vv } };
+        arma::vec b = { b_1, b_2 };
+
+        arma::vec x2 = arma::solve(A, b);
+
+        double u_c = x2[0];
+        double v_c = x2[1];
+        double radius = std::sqrt(u_c * u_c + v_c * v_c + (s_uu + s_vv) / data_size );
+
+        double x_c = u_c + x_avg;
+        double y_c = v_c + y_avg;
+
+        return std::make_tuple(radius, V2d({x_c, y_c}));
     };
 }
 
--- tests/base_test.cpp
+++ tests/base_test.cpp
@@ -9,19 +9,96 @@
 
 using namespace lscf;
 
+// Avg is the (0, 0). Thus just testing radius for r=1.
 TEST(LeastSquareCircleFitTest, BaseCase1) {
-    std::vector<V2d> v2ds({V2d({0, 0}),
-                           V2d({0, 5}),
-                           V2d({1, 6}),
-                           V2d({2, 7}),
-                           V2d({8, 7})});
+    std::vector<V2d> v2ds({V2d({-1, 0}),
+                           V2d({0, 1}),
+                           V2d({1, 0}),
+                           V2d({0, -1}) });
     std::tuple<Radius, V2d> result = least_square_circle_fit(v2ds);
 
     Radius r = std::get<0>(result);
     V2d center = std::get<1>(result);
-    ASSERT_NEAR(30.57261F, r, 0.1);
-    ASSERT_NEAR(4.98735F, std::get<0>(center), 0.1);
-    ASSERT_NEAR(2.34605F, std::get<1>(center), 0.1);
+
+    ASSERT_NEAR(1.0, r, 0.1);
+    ASSERT_NEAR(0.0, std::get<0>(center), 0.1);
+    ASSERT_NEAR(0.0, std::get<1>(center), 0.1);
+}
+
+// Avg of y is not 0. Now testing the center.
+TEST(LeastSquareCircleFitTest, BaseCase2) {
+    std::vector<V2d> v2ds({V2d({-1, 0}),
+                           V2d({0, 1}),
+                           V2d({1, 0}) });
+    std::tuple<Radius, V2d> result = least_square_circle_fit(v2ds);
+
+    Radius r = std::get<0>(result);
+    V2d center = std::get<1>(result);
+
+    ASSERT_NEAR(1.0, r, 0.1);
+    ASSERT_NEAR(0.0, std::get<0>(center), 0.1);
+    ASSERT_NEAR(0.0, std::get<1>(center), 0.1);
+}
+
+// Avg of x is not 0. Now testing the center.
+TEST(LeastSquareCircleFitTest, BaseCase3) {
+    std::vector<V2d> v2ds({V2d({-1, 0}),
+                           V2d({0, 1}),
+                           V2d({0, -1}) });
+    std::tuple<Radius, V2d> result = least_square_circle_fit(v2ds);
+
+    Radius r = std::get<0>(result);
+    V2d center = std::get<1>(result);
+
+    ASSERT_NEAR(1.0, r, 0.1);
+    ASSERT_NEAR(0.0, std::get<0>(center), 0.1);
+    ASSERT_NEAR(0.0, std::get<1>(center), 0.1);
+}
+
+// Avg is the (0, 0). r=1 = alpha=sqrt(r)=1, which is actually a special case. Test for non-special case then.
+TEST(LeastSquareCircleFitTest, BaseCase4) {
+    std::vector<V2d> v2ds({V2d({-2, 0}),
+                           V2d({0, 2}),
+                           V2d({2, 0}),
+                           V2d({0, -2}) });
+    std::tuple<Radius, V2d> result = least_square_circle_fit(v2ds);
+
+    Radius r = std::get<0>(result);
+    V2d center = std::get<1>(result);
+
+    ASSERT_NEAR(2.0, r, 0.1);
+    ASSERT_NEAR(0.0, std::get<0>(center), 0.1);
+    ASSERT_NEAR(0.0, std::get<1>(center), 0.1);
+}
+
+// Avg of x is not 0. r=1 = alpha=sqrt(r)=1, which is actually a special case. Test for non-special case then.
+TEST(LeastSquareCircleFitTest, BaseCase5) {
+    std::vector<V2d> v2ds({V2d({-2, 0}),
+                           V2d({0, 2}),
+                           V2d({0, -2}) });
+    std::tuple<Radius, V2d> result = least_square_circle_fit(v2ds);
+
+    Radius r = std::get<0>(result);
+    V2d center = std::get<1>(result);
+
+    ASSERT_NEAR(2.0, r, 0.1);
+    ASSERT_NEAR(0.0, std::get<0>(center), 0.1);
+    ASSERT_NEAR(0.0, std::get<1>(center), 0.1);
+}
+
+// Avg of y is not 0. r=1 = alpha=sqrt(r)=1, which is actually a special case. Test for non-special case then.
+TEST(LeastSquareCircleFitTest, BaseCase6) {
+    std::vector<V2d> v2ds({V2d({-2, 0}),
+                           V2d({0, 2}),
+                           V2d({2, 0})});
+    std::tuple<Radius, V2d> result = least_square_circle_fit(v2ds);
+
+    Radius r = std::get<0>(result);
+    V2d center = std::get<1>(result);
+
+    ASSERT_NEAR(2.0, r, 0.1);
+    ASSERT_NEAR(0.0, std::get<0>(center), 0.1);
+    ASSERT_NEAR(0.0, std::get<1>(center), 0.1);
 }
 
 GTEST_API_ int main(int argc, char **argv) {
